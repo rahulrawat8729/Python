e13fa842c0824cbd2590e701149c6d315977a1c7
rahulrawat8729 <rahulrawat8729@gmail.com>
2025-08-24 22:36:13 +0530

    first commit

---
+++ Voise Assistant/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+import pyttsx3 #download pip
+import pyaudio #download pyaudio
+import datetime
+import speech_recognition as sr #download speech recognition
+import wikipedia
+import webbrowser
+import os
+import smtplib
+import numpy as np
+import cv2
